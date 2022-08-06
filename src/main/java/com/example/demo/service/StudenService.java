9bab9434b9dcabfc992a8c08f1a8da3ea5352781
UdarbhareKiran <kpujari42@gmail.com>
2022-08-06 18:53:15 +0530

    Adding Somthing

--- src/main/java/com/example/demo/service/StudenService.java
+++ src/main/java/com/example/demo/service/StudenService.java
@@ -1,5 +1,20 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import com.example.demo.model.Student;
+
 public interface StudenService {
+	
+	public int addStudent(Student student);
+	public List<Student>getAllStudent();
+	public Student getEmployeeById(Student id);
+	public Student update(Student student);
+	
+	 public boolean delete(int uid);
+	
+	
+	
+	
 
 }
--- src/main/java/com/example/demo/service/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/StudentServiceImpl.java
@@ -1,5 +1,39 @@
 package com.example.demo.service;
 
-public class StudentServiceImpl {
+import java.util.List;
+
+import com.example.demo.model.Student;
+
+public class StudentServiceImpl implements StudenService {
+
+	@Override
+	public int addStudent(Student student) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Student> getAllStudent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student getEmployeeById(Student id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student update(Student student) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(int uid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
