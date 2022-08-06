e2b07a700153639914a3c39a813efeb3c32ab373
UdarbhareKiran <kpujari42@gmail.com>
2022-08-06 18:32:28 +0530

    Addint ServiceLayer

--- src/main/java/com/example/demo/service/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/StudentServiceImpl.java
@@ -1,5 +1,21 @@
 package com.example.demo.service;
 
-public class StudentServiceImpl {
+import java.util.List;
+
+import com.example.demo.model.Student;
+
+public class StudentServiceImpl implements StudenService {
+
+	@Override
+	public int addStudent(Student student) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Student> getallStudent(Student Integer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
